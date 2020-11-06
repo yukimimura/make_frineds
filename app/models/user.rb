bc82c25d049382b355ad79bc988834a95a675195
三村友幾 <yuki@mimuratomoikunoMacBook-Pro.local>
2020-11-06 13:26:00 +0900

    setUp

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  # 画像アップロード
+  mount_uploader :image, ImageUploader
+end
