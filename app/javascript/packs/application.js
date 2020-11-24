397639655dcdac5ca61e12ba207d0cfc0cd723a1
三村友幾 <yuki@yuki.local>
2020-11-24 23:37:46 +0900

    fix bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,3 +16,6 @@ require('jquery');
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import 'bootstrap'
+import '../stylesheets/application'
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,12 +1,13 @@
 const { environment } = require('@rails/webpacker')
 
-const webpack = require('webpack');
+const webpack = require('webpack')
 
-environment.plugins.append('Provide',
+environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
     $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery'
+    jQuery: 'jquery/src/jquery',
+    Popper: ['popper.js', 'default']
   })
-);
+)
 
 module.exports = environment
